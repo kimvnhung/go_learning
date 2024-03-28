d6d6216ddbeef008131985a173e93e1d56c333d7
kimvnhung <kimvnhung@gmail.com>
2024-03-28 16:30:10 +0700

    save

--- image_render/main.go
+++ image_render/main.go
@@ -16,15 +16,25 @@ func main() {
 	// }
 
 	// dc := gg.NewContextForImage(background)
+	// resized cover : 300x400
+	// background shared : 376x376
 	start := time.Now().UnixMilli()
-	scaleTemp, err := gg.LoadImage("raw_image.jpg")
-	if err != nil {
-		log.Printf("%v", err)
-	}
-	scaleCtx := gg.NewContextForImage(scaleTemp)
-	img := resize.Resize(400, 300, scaleCtx.Image(), resize.Lanczos2)
+	// scaleTemp, err := gg.LoadImage("background.png")
+	// if err != nil {
+	// 	log.Printf("%v", err)
+	// }
+	// scaleCtx := gg.NewContextForImage(scaleTemp)
+	// img := resize.Resize(400, 300, scaleCtx.Image(), resize.Lanczos2)
 
-	dc := gg.NewContextForImage(img)
-	dc.SavePNG("out.png")
+	// dc := gg.NewContextForImage(img)
+	// dc.SavePNG("out.png")
+	background, _ := gg.LoadImage("background.png")
+	backgroundResized := resize.Resize(376, 376, background, resize.Lanczos2)
+	unsplash, _ := gg.LoadImage("raw_image.jpg")
+	unsplashResized := resize.Resize(300, 400, unsplash, resize.Lanczos2)
+	gg.SavePNG("unsplashResized.png", unsplashResized)
+	// gg.SavePNG("backgroundResized.png", backgroundResized)
+	dc := gg.NewContextForImage(backgroundResized)
+
 	log.Printf("time : %d", time.Now().UnixMilli()-start)
 }
