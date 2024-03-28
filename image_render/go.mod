8c1aa35e692d16d44ed55f93c3389619a1360356
kimvnhung <kimvnhung@gmail.com>
2024-03-28 15:43:57 +0700

    scale done

---
+++ image_render/go.mod
@@ -0,0 +1,13 @@
+module github.com/kimvnhung/go_learning/image_render
+
+go 1.22.1
+
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.15.0 // indirect
+)
--- image_render/main.go
+++ image_render/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+
+	"github.com/fogleman/gg"
+	"github.com/nfnt/resize"
+)
 
 func main() {
 	log.Println("Hello")
+	// background, err := gg.LoadImage("output_temp.png")
+	// if err != nil {
+	// 	log.Printf("%v", err)
+	// }
+
+	// dc := gg.NewContextForImage(background)
+	start := time.Now().UnixMilli()
+	scaleTemp, err := gg.LoadImage("raw_image.jpg")
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	scaleCtx := gg.NewContextForImage(scaleTemp)
+	img := resize.Resize(400, 300, scaleCtx.Image(), resize.Lanczos2)
+
+	dc := gg.NewContextForImage(img)
+	dc.SavePNG("out.png")
+	log.Printf("time : %d", time.Now().UnixMilli()-start)
 }
